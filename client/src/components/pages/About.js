e6d8fd3a0064a4dc3957ed6fe69108864a250db0
quidfacitis <majoross@gmail.com>
2020-08-29 11:21:46 -0600

    Add <About /> blurb

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -3,7 +3,9 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <p>About page</p>
+      <p> A full-stack React app that lets users create a profile and keep track of their favorite beers, in addition to discovering new ones from BreweryDB's API.</p>
+      <br></br>
+      <p>A portfolio project by Ross Loney (<a href='https://github.com/quidfacitis' target="_blank" rel="noopener noreferrer">github.com/quidfacitis</a>)</p>
     </div>
   )
 }
